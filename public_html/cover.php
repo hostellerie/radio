3144fd02b76c4cf392843769a7ddc28b8c301c9d
Ben <hostellerie.org@gmail.com>
2026-09-20 12:51:22 +0700

    Add controlled Radio cover endpoint

---
+++ public_html/cover.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../lib-common.php';
+
+$type = isset($_GET['type']) && $_GET['type'] === 'program' ? 'program' : 'media';
+$id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
+
+if ($id < 1 || !RADIO_sendCover($type, $id)) {
+    header('HTTP/1.1 404 Not Found');
+    exit;
+}
