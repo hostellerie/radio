391bc386be57edd20c0230dae27c67116c964b0b
Ben <hostellerie.org@gmail.com>
2026-09-21 21:04:08 +0700

    Fix Radio contract test syntax

--- tests/plugin_api_contract.php
+++ tests/plugin_api_contract.php
@@ -118,7 +118,7 @@ radio_contract_require(
 $adminUi = file_get_contents($root . '/admin/admin-ui.inc.php');
 $mediaTable = file_get_contents($root . '/templates/admin/media-list-table.thtml');
 radio_contract_require(
-    strpos($adminUi, "return $LANG_RADIO['source_local'];") !== false
+    strpos($adminUi, "return \$LANG_RADIO['source_local'];") !== false
         && strpos($adminUi, 'function RADIO_adminSortHeader') !== false
         && strpos($mediaTable, '{modified_header}') !== false,
     'Radio media library must identify local files correctly and expose sortable administrative columns.'
