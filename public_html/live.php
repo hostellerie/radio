4ab08cd0927851800e2fe9cffe4ecf47816d7e6a
Ben <hostellerie.org@gmail.com>
2026-09-21 11:51:03 +0700

    Remove VLC live link from public Radio UI

--- public_html/live.php
+++ public_html/live.php
@@ -27,8 +27,7 @@ $content = '<div class="radio-public"><section class="radio-player-card radio-pl
     . htmlspecialchars($LANG_RADIO['back_to_library'], ENT_QUOTES, 'UTF-8') . '</a>'
     . '<a href="' . htmlspecialchars($_CONF['site_url'] . '/radio/schedule.php', ENT_QUOTES, 'UTF-8') . '">'
     . htmlspecialchars($LANG_RADIO['view_full_schedule'], ENT_QUOTES, 'UTF-8') . '</a>'
-    . '<a href="' . htmlspecialchars($_CONF['site_url'] . '/radio/vlc.php', ENT_QUOTES, 'UTF-8') . '">'
-    . htmlspecialchars($LANG_RADIO['open_in_vlc'], ENT_QUOTES, 'UTF-8') . '</a></nav>'
+</nav>'
     . '</section></div>';
 
 COM_output(COM_createHTMLDocument($content, array(
