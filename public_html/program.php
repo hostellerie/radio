e6a290a2503569c7e615d549a7463d604ca7f3df
Ben <hostellerie.org@gmail.com>
2026-09-21 14:08:58 +0700

    Keep Radio programme pages editorial only

--- public_html/program.php
+++ public_html/program.php
@@ -12,7 +12,6 @@ if ($program === false) {
     exit;
 }
 
-$items = RADIO_getProgramItems($id);
 $content = '<div class="radio-program"><h1>' . htmlspecialchars($program['title'], ENT_QUOTES, 'UTF-8') . '</h1>';
 if (!empty($program['cover_name'])) {
     $content .= '<p><img src="' . htmlspecialchars(RADIO_coverUrl('program', $id), ENT_QUOTES, 'UTF-8') . '" alt="" style="max-width:360px;width:100%;height:auto"></p>';
@@ -22,22 +21,6 @@ if (!empty($program['host'])) {
 }
 if ($program['description'] !== '') {
     $content .= '<p>' . nl2br(htmlspecialchars($program['description'], ENT_QUOTES, 'UTF-8')) . '</p>';
-}
-if (count($items) === 0) {
-    $content .= '<p>' . htmlspecialchars($LANG_RADIO['program_public_empty'], ENT_QUOTES, 'UTF-8') . '</p>';
-} else {
-    $content .= '<ol>';
-    foreach ($items as $item) {
-        if (!RADIO_isOnDemandAvailable($item) || !RADIO_hasReadAccess($item)) {
-            continue;
-        }
-        $content .= '<li style="margin:0 0 1.25rem"><strong>'
-            . htmlspecialchars($item['title'], ENT_QUOTES, 'UTF-8') . '</strong><br>'
-            . '<audio data-radio-media-id="' . (int) $item['media_id'] . '" data-radio-program-id="' . (int) $id . '" data-radio-source="program" controls preload="none" style="width:100%;max-width:800px" src="'
-            . htmlspecialchars(RADIO_mediaUrl((int) $item['media_id'], false), ENT_QUOTES, 'UTF-8')
-            . '"></audio></li>';
-    }
-    $content .= '</ol>';
 }
 $content .= '<p><a href="' . htmlspecialchars($_CONF['site_url'] . '/radio/replay.php', ENT_QUOTES, 'UTF-8') . '">'
     . htmlspecialchars($LANG_RADIO['replays'], ENT_QUOTES, 'UTF-8') . '</a> · '
