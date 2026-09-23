a0d6080281faf4102721d79459f178ddc4f2205e
Ben <hostellerie.org@gmail.com>
2026-09-23 11:19:09 +0700

    Add persistent player link to Radio page

--- public_html/index.php
+++ public_html/index.php
@@ -100,6 +100,8 @@ if ($liveState['media'] !== false) {
         . '</div>'
         . '</div>'
         . '<nav class="radio-player-links" aria-label="' . htmlspecialchars($LANG_RADIO['plugin_name'], ENT_QUOTES, 'UTF-8') . '">'
+        . '<a href="' . htmlspecialchars($_CONF['site_url'] . '/radio/player.php', ENT_QUOTES, 'UTF-8') . '" data-radio-persistent-player>'
+        . htmlspecialchars($LANG_RADIO['persistent_listen'], ENT_QUOTES, 'UTF-8') . '</a>'
         . '<a href="' . htmlspecialchars($_CONF['site_url'] . '/radio/schedule.php', ENT_QUOTES, 'UTF-8') . '">'
         . htmlspecialchars($LANG_RADIO['view_full_schedule'], ENT_QUOTES, 'UTF-8') . '</a>'
         . '<a href="' . htmlspecialchars(RADIO_podcastFeedUrl(), ENT_QUOTES, 'UTF-8') . '">'
