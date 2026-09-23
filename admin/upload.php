023c3d5629716eaa047bfa552dd57f57c3aaa7d5
Ben <hostellerie.org@gmail.com>
2026-09-23 16:32:05 +0700

    Expose automatic rotation during uploads

--- admin/upload.php
+++ admin/upload.php
@@ -154,6 +154,8 @@ $template->set_var(array(
     'status_label' => htmlspecialchars($LANG_RADIO['status'], ENT_QUOTES, 'UTF-8'),
     'on_demand_label' => htmlspecialchars($LANG_RADIO['on_demand'], ENT_QUOTES, 'UTF-8'),
     'broadcast_label' => htmlspecialchars($LANG_RADIO['broadcast'], ENT_QUOTES, 'UTF-8'),
+    'automatic_rotation_label' => htmlspecialchars($LANG_RADIO['automatic_rotation'], ENT_QUOTES, 'UTF-8'),
+    'automatic_rotation_help' => htmlspecialchars($LANG_RADIO['automatic_rotation_help'], ENT_QUOTES, 'UTF-8'),
     'allow_download_label' => htmlspecialchars($LANG_RADIO['allow_download'], ENT_QUOTES, 'UTF-8'),
     'permissions_label' => htmlspecialchars($LANG_RADIO['permissions'], ENT_QUOTES, 'UTF-8'),
     'group_label' => htmlspecialchars($LANG_RADIO['group'], ENT_QUOTES, 'UTF-8'),
