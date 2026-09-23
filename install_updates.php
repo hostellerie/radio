e2a9221db510f2baf6e6077924480da7e6b6e134
Ben <hostellerie.org@gmail.com>
2026-09-23 16:05:35 +0700

    Add Radio 0.3.0 to 0.3.1 upgrade path

--- install_updates.php
+++ install_updates.php
@@ -7,6 +7,10 @@ $GLOBALS['RADIO_UPDATES'] = array(
     '0.2.5' => array(
         'next' => '0.3.0',
         'callback' => 'radio_update_0_2_5_to_0_3_0'
+    ),
+    '0.3.0' => array(
+        'next' => '0.3.1',
+        'callback' => 'radio_update_0_3_0_to_0_3_1'
     )
 );
 
@@ -46,6 +50,13 @@ function radio_update_0_2_5_to_0_3_0()
 
     return radio_column_exists($table, 'on_demand')
         && radio_column_exists($table, 'broadcast');
+}
+
+function radio_update_0_3_0_to_0_3_1()
+{
+    // No schema migration is required. plugin_upgrade_radio() reconciles
+    // the existing Geeklog configuration through RADIO_ensureConfig().
+    return true;
 }
 
 function radio_apply_updates($installedVersion, $targetVersion)
