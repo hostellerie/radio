689cf7eebde87d34faace2aa81db94fd5bfb3020
Ben <hostellerie.org@gmail.com>
2026-09-24 18:01:31 +0700

    Redirect disabled Radio public page to site index

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../lib-common.php';
-RADIO_requirePublicAccess();
+RADIO_requirePublicAccess(true);
 global $_TABLES, $_CONF, $_RADIO_CONF, $LANG_RADIO;
 
 $title = isset($_RADIO_CONF['public_title']) ? $_RADIO_CONF['public_title'] : 'Radio';
