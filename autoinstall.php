5df085f776999dd22565d0b5ded8cdd41d8cfa86
Ben <hostellerie.org@gmail.com>
2026-09-24 13:52:00 +0700

    Remove obsolete shared catalogue table from installer

--- autoinstall.php
+++ autoinstall.php
@@ -32,7 +32,6 @@ function plugin_autoinstall_radio($pi_name)
         ),
         'tables' => array(
             'radio_media',
-            'radio_site_media',
             'radio_programs',
             'radio_program_items',
             'radio_schedule',
