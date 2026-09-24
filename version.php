c35b7bddae0a56bee224a317ef1bc17fef7fb360
Ben <hostellerie.org@gmail.com>
2026-09-24 22:43:27 +0700

    Start Radio 0.6.0 semi-live development

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 if (!isset($GLOBALS['_CONF'])) {
     die('This file cannot be used on its own.');
 }
-define('RADIO_PLUGIN_VERSION', '0.5.1');
+define('RADIO_PLUGIN_VERSION', '0.6.0');
 define('RADIO_MIN_GEEKLOG_VERSION', '2.1.1');
 define('RADIO_MIN_PHP_VERSION', '5.6.0');
 define('RADIO_RELEASE_STATUS', 'development');
